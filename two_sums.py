d0a7b6439b4bdd7771c2fd105c36e5dddcd99db1
tarique313 <mahmud.tarique@gmail.com>
2022-12-07 21:26:02 +0600

    Find two sums

---
+++ two_sums.py
@@ -0,0 +1,12 @@
+from ast import List
+
+class Solution:
+    def twoSums(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
